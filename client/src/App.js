8fb40d0e5357e57a489a5f6af64adcf027e71aa4
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-01-25 16:51:44 -0500

    done modifing index, app and movie

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+//import router
+import {Route} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+//import MovieList
+import MovieList from './Movies/MovieList';
+
+//import { Router } from 'express';
+import Movie from './Movies/Movie';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +21,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -29,8 +37,14 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <Route path='/'>
+        <MovieList movies = {movieList} />
+        {/*<div>Replace this Div with your Routes</div> --- this was given*/}
+      </Route>
+      <Route path={'/movies/:id'}>
+        <Movie />
+      </Route>
+      
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//import react router
+import {BrowserRouter as Router } from 'react-router-dom'
+
 import './index.css';
 import App from './App';
 
 // You'll need to wrap <App /> for routing to work
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        {/*ReactDOM.render(<App />, document.getElementById('root')); -----this was given*/}
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
